d3ae3f144749f36e6162f1139a1212482a4df112
Jessica Ellingson <jessica@jessicaellingson.com>
2011-03-02 21:12:29 -0600

    finished portfolio, change blog link

--- site.js
+++ site.js
@@ -1,8 +1,8 @@
 $(function() {
   $('.thumb').click(function(e) {
+    e.preventDefault();
     image = $('#main-image');
-    image_name = e.currentTarget.src.split('/').pop();
+    image_name = e.currentTarget.href.split('/').pop();
     $('#main-image').attr('src', '/images/portfolio/' + image_name);
-    console.log(image_name);
   });
 });
